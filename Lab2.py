aed61d275c51a4be22f244414b12db8718e98a8a
KhinMayHtet <khinmhhtet05.24@ichat.sp.edu.sg>
2025-11-04 14:59:43 +0800

    Initial Version 2

--- Lab2.py
+++ Lab2.py
@@ -1,2 +1,39 @@
+# def calculate_bmi(height,weight):
+#     print("Height =" + str(height))
+#     print("Weight =" + str(weight))
+#     bmi= weight/(height * height)
+#     print("BMI = " + str(bmi))
+
+#     if bmi< 18.5:
+#         print("Underweight")
+        
+#     elif bmi >= 18.5 and bmi < 24.9:
+#         print("Normal weight")
+
+#     else:4
+#         print("Overweight")
 
 
+
+# def display_main_menu(numbers):
+#     print(numbers)
+#     numbers = input("Enter a number between 1 to 5: ")
+#     return numbers
+
+# def main():
+#     print("ET0735(DevOps for AIoT)-Lab2 - Introdiction to Python")
+#     a = display_main_menu(0)
+#     print(a)
+# if __name__ == "__main__":
+#     main()
+
+
+# print ('Enter your name:')
+# name = input ()
+# print ('Hello, ' + name)
+
+txt = "welcome to the jungle"
+
+x = txt.split()
+
+print(x)
--- bmi.py
+++ bmi.py
@@ -1,18 +1,29 @@
-def calculate_bmi(height,weight):
-    print("Height =" + str(height))
-    print("Weight =" + str(weight))
 
-    bmi= weight / (height * height)
-    print("BMI = " + str(bmi))
 
-    if bmi< 18.5:
-        print("Underweight")
-        
-    elif bmi >= 18.5 and bmi < 24.9:
+def calculate_bmi(height, weight):
+    bmi = weight / (height * height)
+
+    if bmi < 18.5:
+        print ("Underweight")
+        return -1    
+    elif 18.5 <= bmi < 24.9:
         print("Normal weight")
-
+        return 0
     else:
         print("Overweight")
+        return 1
+
+    return bmi
+
+def main():
+    height = float(input("Enter height"))
+    weight = float(input("Enter weight"))
+    bmi = calculate_bmi(height, weight)
+    print("BMI = ", str(round(bmi,2)))
+
+if __name__ == "__main__":
+    main()
+    
 
 
-calculate_bmi(1.75, 108)
+
